578b1abb4d32dc63c783a559bc41102b64cf8d25
LudovicBraine <ludovic.braine@gmail.com>
2022-10-05 00:23:56 +0200

    add new api routes

--- project/src/Controller/TaskController.php
+++ project/src/Controller/TaskController.php
@@ -9,33 +9,94 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 use App\Entity\Task;
+use Exception;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class TaskController extends AbstractController
 {
-    #[Route('/tasks', methods: 'GET')]
+    #[Route('/api/tasks', methods: 'GET')]
     public function alltasks(TaskRepository $taskRepository): Response
     {        
-        return $response = $this->json($taskRepository->findAll(), 200, [], ['groups' => 'api_tasks']);
+        return $this->json($taskRepository->findAll(), 200, [], ['groups' => 'api_tasks']);
     }
 
-    #[Route('/tasks', methods: 'POST')]
-    public function createTask(Request $request, TaskRepository $taskRepository, SerializerInterface $serializer, EntityManagerInterface $em): Response
+    #[Route('/api/tasks', methods: 'POST')]
+    public function createTask(Request $request, EntityManagerInterface $em): Response
     { 
         $resolver = new OptionsResolver();
         $resolver->setRequired(['title', 'description'])
             ->setAllowedTypes('title', 'string')
             ->setAllowedTypes('description', 'string');
+        $resolver->resolve($request->request->all());
 
-        $data = $request->getContent();
+        $data = $request->request->all();
+    
+        $task = new Task();
+        $task->setTitle($data['title']);
+        $task->setDescription($data['description']);
+    
+        try {
+            $em->persist($task);
+            $em->flush();
+        
+            return $this->json($task, 201, [], ['groups' => 'api_tasks']);
+        } catch (Exception $e) {
+            return $this->json([
+                'status' => 400,
+                'message' => $e->getMessage()
+            ], 400);
+        }
+    }
 
-        $task = $serializer->deserialize($data, Task::class, 'json');
+    #[Route('/api/tasks/{id}', methods: 'PUT')]
+    #[ParamConverter("task", class: Task::class, options: ['id' => 'id'])]
+    public function editTask(Task $task, Request $request, EntityManagerInterface $em): Response
+    { 
+        $resolver = new OptionsResolver();
+        $resolver->setRequired(['title', 'description', 'state'])
+            ->setAllowedTypes('title', 'string')
+            ->setAllowedTypes('description', 'string')
+            ->setAllowedTypes('state', 'string')
+            ->setAllowedValues('state', ['TASK_OPEN', 'TASK_FINISHED', 'TASK_LATE']);
 
-        $em->persist($task);
-        $em->flush();
-        dd($task);
+        $resolver->resolve($request->request->all());
 
-        return $response = $this->json($taskRepository->findAll(), 200, [], ['groups' => 'api_tasks']);
+        $data = $request->request->all();
+
+        $task->setTitle($data['title']);
+        $task->setDescription($data['description']);
+        $task->setState($data['state']);
+        $task->setUpdatedAt(new \DateTimeImmutable());
+
+        try {
+            $em->persist($task);
+            $em->flush();
+            return $this->json($task, 201, [], ['groups' => 'api_tasks']);
+
+        } catch (Exception $e){
+            return $this->json([
+                'status' => 400,
+                'message' => $e->getMessage()
+            ], 400);
+        }
+
+    }
+
+    #[Route('/api/tasks/{id}', methods: 'DELETE')]
+    #[ParamConverter("task", class: Task::class, options: ['id' => 'id'])]
+    public function removeTask(Task $task, Request $request, EntityManagerInterface $em): Response
+    { 
+        try {
+            $em->remove($task);
+            $em->flush();
+            return new Response('', 200);
+        } catch (Exception $e){
+            return $this->json([
+                'status' => 400,
+                'message' => $e->getMessage()
+            ], 400);
+        }
+
     }
 }
