44b9ab17bb187d249603f63fc8b50b56eb5e16ba
LudovicBraine <ludovic.braine@gmail.com>
2022-10-05 10:39:58 +0200

    WIP: tests

---
+++ project/tests/Controller/TaskController/AllTasksTest.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace App\Tests\Controller\TaskController\allTasksTest;
+
+use App\Factory\TaskFactory;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AllTasksTest extends WebTestCase
+{
+}
